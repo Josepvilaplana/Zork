efdc2a8572267b14d543deb08bdd27d7db68fb83
Josep Vilaplana <josepvilamir@gmail.com>
2023-09-22 18:38:49 +0200

    Basic main
    Main created with only a string to get player actions and an infinite loop that reads the player actions.

---
+++ Zork/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	cout << "Welcome <TODO>" << endl;
+	string action; //Player action
+
+	while (true) {
+		cin >> action;
+		
+	}
+
+	return 0;
+}
